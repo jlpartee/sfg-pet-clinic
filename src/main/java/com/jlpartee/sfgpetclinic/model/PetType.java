cd04d581f479eb5689729219f64e95e182719f32
Johan Partee <jlpartee@me.com>
2018-12-24 09:15:11 -0600

    adding POJOs

---
+++ src/main/java/com/jlpartee/sfgpetclinic/model/PetType.java
@@ -0,0 +1,15 @@
+package com.jlpartee.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
